685f1f453cbff816afea1abd3867fa094a0a293a
Karem Cristine de Souza <karem@clickmulti.com.br>
2021-04-18 16:03:55 -0300

    funcoes do crud

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,121 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <label >ID: </label><input type="text" v-model="id"><br />
+    <label >Tarefa: </label><input type="text" v-model="tarefa"><br />
+    <label >Autor: </label><input type="text" v-model="autor"><br />
+    <button @click="cadastrar">Cadastrar</button>
+       <button @click="buscarposts">Buscar</button>
+            <button @click="atualizarpost">Atualizar</button>
+             <button @click="deletePost">Excluir</button>
+       <hr>
+     <ul>
+       <li v-for="post in posts" :key="post.id">
+       
+         ID: {{post.id}}   <br />
+         Tarefa: {{post.tarefa}}   <br />
+         Autor: {{post.autor}}
+         <br />
+         <br />
+       </li>
+     </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import firebase from './services/firebase-connection';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data(){
+    return{
+      id:'',
+      autor : '',
+      tarefa: '',
+      posts:[]
+    }
+  },
+ async created(){
+   await firebase.firestore().collection('posts')
+   .onSnapshot((doc) => {
+     this.posts = [] 
+     doc.forEach((item) => {
+        this.posts.push({
+         id:item.id,
+         tarefa:item.data().tarefa,
+         autor: item.data().autor
+     })
+     })
+   })
+  },
+  methods:{
+   async cadastrar(){
+      await firebase.firestore().collection('posts')
+      // .doc('123')
+      .add({
+        tarefa:this.tarefa,
+        autor:this.autor
+      })
+      .then( () => {
+        console.log('cadastrado com sucesso')
+        this.autor = ''
+        this.tarefa = ''
+      })
+      .catch((error)=> {
+              console.log('GEROU ERRO',error)
+      })
+    },
+    async buscarposts () {
+      this.posts = []
+      // await firebase.firestore().collection('posts')
+      // .doc('123')
+      // .get()
+      // .then((snapshot) => {
+      //   this.tarefa = snapshot.data().tarefa,
+      //   this.autor = snapshot.data().autor
+      // })
+      // .catch((error) => {
+      //   console.log('GEROU ERRO',error)
+      // })
+      await firebase.firestore().collection('posts')
+      .get()
+      .then ((snapshot) => {
+        snapshot.forEach((doc) => {
+          this.posts.push({
+          id:doc.id,
+          tarefa: doc.data().tarefa,
+          autor: doc.data().autor
+          })
+    
+        })
+      })
+    }, 
+    async atualizarpost(){
+      await firebase.firestore().collection('posts').doc(this.id)
+      .update({
+        tarefa:this.tarefa,
+        autor:this.autor
+      })
+      .then(() => {
+           console.log('atualizado com sucesso')
+                 this.id = ''
+                this.autor = ''
+                 this.tarefa = ''
+      })
+      .catch((error)=> {
+              console.log('GEROU ERRO',error)
+      })
+    },
+    async deletePost(){
+      if(this.id === '') return
+      await firebase.firestore().collection('posts').doc(this.id)
+      .delete()
+      .then (() => {
+        this.id = ''
+        console.log("EXCLUIDO")
+      })
+    }
   }
 }
 </script>
---
+++ src/services/firebase-connection.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBx0bdSDfTS58MRCTOtnnRT-AZKdiagIRk",
+    authDomain: "vueapp-4d71b.firebaseapp.com",
+    projectId: "vueapp-4d71b",
+    storageBucket: "vueapp-4d71b.appspot.com",
+    messagingSenderId: "508813664095",
+    appId: "1:508813664095:web:f0b271b7961e247e4060ea",
+    measurementId: "G-1DDV2CDLXK"
+  };
+  // Initialize Firebase
+  if(firebase.app.length){
+  firebase.initializeApp(firebaseConfig)
+  }
+  export default firebase;
